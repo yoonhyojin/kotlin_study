f66e1a9acae6fca4fd81b5e5f16c3c5b1dc4ef2c
yoonhyojin <dbsgywls9855@gmail.com>
2024-04-27 14:31:25 +0900

    var

---
+++ src/variable.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var number = 20  // 변경 가능함
+    println(number)
+    number = 10
+    println(number)
+    var name = "효진"  // 변수 처음 선언시에는 var을 선언해야함
+    println(name)
+    name = "진효"
+    println(name)
+
+    //변수는 데이터타입을 명시하기 싫을 때도 사용
+
+}
