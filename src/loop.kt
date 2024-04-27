9125a1382723fac673e1a66a64532da5b69a5842
yoonhyojin <dbsgywls9855@gmail.com>
2024-04-27 15:44:34 +0900

    loop

---
+++ src/loop.kt
@@ -0,0 +1,22 @@
+fun main() {
+    //for
+    for(i in 0 ..  10) {
+        println(i)
+    }
+
+    //while
+    var j : Int = 0
+    while(j < 5) {
+        println(j)
+        j++
+    }
+    println(j)
+
+    //do ~ while
+    do {
+        println(j)
+        j++
+    } while (j < 5)
+    println(j)
+
+}
