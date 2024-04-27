507675171579560a76e4afe8cc6ce548f1364ef7
yoonhyojin <dbsgywls9855@gmail.com>
2024-04-27 15:27:09 +0900

    nullSafety

---
+++ src/nullSafety.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var age : Int? = null // ?가 int값이 오지 않았을 때 null값을 자동으로 넣어줌
+    if (age != null) {  // age가 null이 아닐 때만 사용해라
+        println(age)
+        //println(age!!)은 null이여도 강제 수행되게끔 함
+
+    }
+
+}
