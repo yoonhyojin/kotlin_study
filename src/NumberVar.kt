7e40ea546c6b4a4837aa245064dc7d3044603c7a
yoonhyojin <dbsgywls9855@gmail.com>
2024-04-27 14:36:00 +0900

    numberval

---
+++ src/NumberVar.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // 변수 선언시 타입이 고정되면 다른 타입으로 변경 할 수 없음
+    // 타입 선언하지 않으면 자동으러 int로 고정
+
+    var number1 : Int = 20
+    var number2 : Double = 2.0
+    var number3 : Long = 100
+
+
+
+}
